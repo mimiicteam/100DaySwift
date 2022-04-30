8a1d824b54242429224a91ff3e92ad66f0b42403
NGUYEN MINH DUC <mimiicteam@gmail.com>
2022-04-30 08:17:04 +0700

    100 Day Swift

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by NGUYEN MINH DUC on 27/04/2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedPicture: String!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = selectedPicture
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        if let imageToLoad = selectedPicture {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by NGUYEN MINH DUC on 27/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.cornerRadius = 10
+        button2.layer.cornerRadius = 10
+        button3.layer.cornerRadius = 10
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+    }
+
+    func askQuestion(action: UIAlertAction! = nil) {
+//        print("Call Function askQuestion!")
+        correctAnswer = Int.random(in: 0...2)
+        countries.shuffle()
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "You score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: title, style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+    }
+    
+
+}
+
